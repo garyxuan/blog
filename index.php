a9c7d6381437e74001655c0fac7748a71b5f8dcb
zizaimengzhongyue <luyanfeng1992@qq.com>
2016-06-06 17:12:39 +0800

    增加一个主页面

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: yanfeng
- * Date: 2016/6/1
- * Time: 11:29
- */
-echo "This is my first commit";
---
+++ php/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yanfeng
+ * Date: 2016/6/6
+ * Time: 15:20
+ */
